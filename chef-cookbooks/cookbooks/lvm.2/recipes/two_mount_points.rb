73a211e41d994c025e7ded84ff8c0dfea1eeb667
root <root@wzxdcc2196.cdbt.wcdc.kp.org>
2014-03-04 15:48:25 -0800

    Edit LVM v1.0.6

--- chef-cookbooks/cookbooks/lvm.2/recipes/two_mount_points.rb
+++ chef-cookbooks/cookbooks/lvm.2/recipes/two_mount_points.rb
@@ -27,28 +27,29 @@
 # Copyright 2009-2014, Alfred Shen.
 
 lvm_volume_group 'vg00' do
-  physical_volumes ['/dev/sda', '/dev/sdb', '/dev/sdc']
+  physical_volumes ['/dev/sdb', '/dev/sdc']
 
   logical_volume 'ora_log' do
-    size        '2G'
-    filesystem  'xfs'
+    size        '2%VG'
+#    filesystem  'xfs'
+    filesystem  'ext4'
     mount_point location: '/var/oracle/logs', options: 'noatime,nodiratime'
-    stripes     3
+    stripes     2
   end
 
   logical_volume 'ora01' do
-    size        '25%VG'
+    size        '50%FREE'
     filesystem  'ext4'
     mount_point '/ora01'
-    stripes     3
-    mirrors     2
+#    stripes     2
+#    mirrors     1
   end
 
   logical_volume 'ora02' do
-    size        '25%VG'
+    size        '100%FREE'
     filesystem  'ext4'
     mount_point '/ora02'
-    stripes     3
-    mirrors     2
+#    stripes     2
+#    mirrors     1
   end
 end
