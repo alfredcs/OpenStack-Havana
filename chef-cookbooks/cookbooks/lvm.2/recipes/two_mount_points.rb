9001c2d0a7cceb8af9db6f1167a1be4f92c6dba8
root <root@wzxdcc2196.cdbt.wcdc.kp.org>
2014-03-04 10:29:14 -0800

    Modified LVM

--- chef-cookbooks/cookbooks/lvm.2/recipes/two_mount_points.rb
+++ chef-cookbooks/cookbooks/lvm.2/recipes/two_mount_points.rb
@@ -1,3 +1,31 @@
+#
+# Cookbook Name:: lvm
+# Recipe::two_mount_points
+#
+# Related cmds
+# knife node show <host_name>
+# knife node run_list add <host_name> 'role[create-lvm]'
+# knife node edit <host_name>
+# knife node show <host_name>
+# knife role edit create-lvm
+# knife node edit <host_name>
+# knife cookbook upload lvm
+# knife cookbook test lvm
+# knife role show create-lvm
+#chef_type:           role
+#default_attributes:
+#description:         Post installation configuration for lvm to be used by applicaiton including Oracle
+#env_run_lists:
+#json_class:          Chef::Role
+#name:                create-lvm
+#override_attributes:
+#run_list:
+#  role[base]
+#  recipe[lvm::default]
+#  recipe[lvm::two_mount_points] 
+#
+# Copyright 2009-2014, Alfred Shen.
+
 lvm_volume_group 'vg00' do
   physical_volumes ['/dev/sda', '/dev/sdb', '/dev/sdc']
 
